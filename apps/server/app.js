d40a34f96ee9a930e4f151913e8cecd5543ce061
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-02 05:28:27 +0000

    feat: server connect with database

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,6 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
+
+// middleware setup 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(cors());
 
 
 export default app;
--- apps/server/index.js
+++ apps/server/index.js
@@ -1,7 +1,15 @@
 import app from "./app.js";
+import connectDB from "./db/connect.js";
 import { port } from "./config/env.js";
 
 // server listen
-app.listen(port, () => {
-  console.log(`listening on port ${port}`);
-});
+connectDB()
+.then(()=>{
+  app.listen(port, () => {
+    console.log(`listening on port ${port}`);
+  });
+})
+.catch((error)=>{
+  console.log("unable to connect to database", error);
+  process.exit(1);
+})
