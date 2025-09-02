922fc8758d54e16e15fd55a57e004866d044913c
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-02 07:55:34 +0000

    feat(server): jwt secret and cookies options configure

--- apps/server/config/env.js
+++ apps/server/config/env.js
@@ -4,9 +4,41 @@ const _config = Object.freeze({
   nodeEnv: process.env.NODE_ENV,
   googleClientId: process.env.GOOGLE_CLIENT_ID,
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL
+  googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL,
+  accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
+  accessTokenExpiresIn: process.env.ACCESS_TOKEN_EXPIRES_IN,
+  refreshTokenSecret: process.env.REFRESH_TOKEN_SECRET,
+  refreshTokenExpiresIn: process.env.REFRESH_TOKEN_EXPIRES_IN,
 });
 
-const { port, mongoUrl, nodeEnv, googleClientId, googleClientSecret,googleCallbackUrl } = _config;
+const {
+  port,
+  mongoUrl,
+  nodeEnv,
+  googleClientId,
+  googleClientSecret,
+  googleCallbackUrl,
+  accessTokenExpiresIn,
+  accessTokenSecret,
+  refreshTokenExpiresIn,
+  refreshTokenSecret,
+} = _config;
 
-export { port, mongoUrl, nodeEnv, googleClientId, googleClientSecret,googleCallbackUrl };
+export {
+  port,
+  mongoUrl,
+  nodeEnv,
+  googleClientId,
+  googleClientSecret,
+  googleCallbackUrl,
+  accessTokenExpiresIn,
+  accessTokenSecret,
+  refreshTokenExpiresIn,
+  refreshTokenSecret,
+};
+
+// cookies options
+export const cookiesOptions = Object.freeze({
+  httpOnly: true,
+  secure: nodeEnv === "production" ? true : false,
+});
