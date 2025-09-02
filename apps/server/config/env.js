02b9c295dce0eab062e9595daada8bf74d3ab710
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-02 07:05:58 +0000

    feat(sever): passport using google strategy configure

--- apps/server/config/env.js
+++ apps/server/config/env.js
@@ -2,8 +2,11 @@ const _config = Object.freeze({
   port: process.env.PORT,
   mongoUrl: process.env.MONGODB_URI,
   nodeEnv: process.env.NODE_ENV,
+  googleClientId: process.env.GOOGLE_CLIENT_ID,
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  googleCallbackUrl: process.env.GOOGLE_CALLBACK_URL
 });
 
-const { port, mongoUrl, nodeEnv } = _config;
+const { port, mongoUrl, nodeEnv, googleClientId, googleClientSecret,googleCallbackUrl } = _config;
 
-export { port, mongoUrl, nodeEnv };
+export { port, mongoUrl, nodeEnv, googleClientId, googleClientSecret,googleCallbackUrl };
