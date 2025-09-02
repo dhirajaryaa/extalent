54222b4b405b4f44c096a77957b4d0c9c745d277
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-02 05:46:21 +0000

    feat(server): server global Error handler

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,13 +1,20 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 
-// middleware setup 
+// middleware setup
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static('public'));
+app.use(express.static("public"));
 app.use(cors());
 
+//* Error Handler */
+app.use((err, req, res, next) => {
+  console.error("💥 Oops! Something went wrong:", err.stack); // Log the error for debugging
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({ ...err, message });
+});
 
 export default app;
