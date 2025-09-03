0bd8bdd04bf314c2a90e6836660cf0a53f7f1a78
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-03 23:49:24 +0000

    feat(server): resume text extractor services building

---
+++ apps/server/services/resume.service.js
@@ -0,0 +1,9 @@
+import pdf from "pdf-parse";
+import fs from "node:fs/promises";
+
+const parseResume = async (fileLocalPath) => {
+  if (!fileLocalPath) return;
+  const bufferPdf = await fs.readFile(fileLocalPath);
+  const data = await pdf(bufferPdf);
+  return data.text;
+};
