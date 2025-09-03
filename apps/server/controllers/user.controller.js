dcb0907a51201584186b32573c001d8a2124b602
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-03 00:58:35 +0000

    feat(server):  remove previous pdf first and then save on db new pdf info

--- apps/server/controllers/user.controller.js
+++ apps/server/controllers/user.controller.js
@@ -2,7 +2,10 @@ import AsyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/apiError.js";
 import ApiResponse from "../utils/apiResponse.js";
 import userModal from "../model/user.model.js";
-import { uploadOnCloudinary } from "../services/cloudinary.js";
+import {
+  removeFromCloudinary,
+  uploadOnCloudinary,
+} from "../services/cloudinary.js";
 
 const userProfile = AsyncHandler(async (req, res) => {
   if (!req.user) {
@@ -24,27 +27,28 @@ const userResumeUpload = AsyncHandler(async (req, res) => {
   if (!uploadedResume) {
     throw new ApiError(500, "failed to upload! try again");
   }
-  const thumbnailUrl = `https://res.cloudinary.com/dhirajarya80/image/upload/f_auto,q_auto,pg_1/${uploadedResume.public_id}`
-  // save on db
-  const user = await userModal.findByIdAndUpdate(
-    req.user._id,
-    {
-      resume: {
-        publicId: uploadedResume.public_id,
-        url: uploadedResume.secure_url,
-        thumbnailUrl
-      },
+  const thumbnailUrl = `https://res.cloudinary.com/dhirajarya80/image/upload/f_auto,q_auto,pg_1/${uploadedResume.public_id}`;
+  // get user info
+  const user = await userModal
+    .findById(req.user._id)
+    .select("_id name email resume");
+  // remove previous resume
+  if (user.resume.publicId) {
+    await removeFromCloudinary(user.resume.publicId);
+  }
+  // update on db
+  await userModal.findByIdAndUpdate(req.user._id, {
+    resume: {
+      publicId: uploadedResume.public_id,
+      url: uploadedResume.secure_url,
+      thumbnailUrl,
     },
-    {
-      new: true,
-      select: "_id name email avatar",
-    }
-  );
+  });
 
   return res.status(200).json(
     new ApiResponse(200, "Resume uploaded successfully", {
-      user,
-      resume: {pdf:uploadedResume.secure_url,preview:thumbnailUrl },
+      user: { _id: user._id, name: user.name, email: user.email },
+      resume: { pdf: uploadedResume.secure_url, preview: thumbnailUrl },
     })
   );
 });
