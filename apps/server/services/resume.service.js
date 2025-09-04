5745a4c8bf0a5d8d9e38f15250ff9350eb650834
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-04 13:19:29 +0000

    feat(server): pdf text extractor services create

--- apps/server/services/resume.service.js
+++ apps/server/services/resume.service.js
@@ -5,5 +5,7 @@ const parseResume = async (fileLocalPath) => {
   if (!fileLocalPath) return;
   const bufferPdf = await fs.readFile(fileLocalPath);
   const data = await pdf(bufferPdf);
+  
   return data.text;
 };
+export default parseResume;
