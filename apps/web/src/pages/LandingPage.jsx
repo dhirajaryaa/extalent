d3199ebeabb4a5a6ed5bbf03ef290b6d20029e74
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-07 15:28:56 +0000

    style(web): create landing page

---
+++ apps/web/src/pages/LandingPage.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function LandingPage() {
+  return (
+    <main className='w-full min-h-screen max-w-4xl mx-auto'>
+        <section className='w-full h-full bg-red-300'>
+            <nav>Extalent</nav>
+        </section>
+    </main>
+  )
+}
+
+export default LandingPage
