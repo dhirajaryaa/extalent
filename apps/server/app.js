9d606351064e17a08f0765f483ca34c9fbdb195f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-08 15:04:57 +0000

    refactor(server): cors origin setup

--- apps/server/app.js
+++ apps/server/app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import cors from "cors";
-import passport from './config/passport.js';
+import passport from "./config/passport.js";
 import cookieParser from "cookie-parser";
+import { allowOrigin } from "./config/env.js";
 
 const app = express();
 
@@ -9,15 +10,23 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.use(cors());
-app.use(cookieParser())
+app.use(cookieParser());
 app.use(passport.initialize());
+app.use(
+  cors({
+    origin: allowOrigin,
+   credentials: true
+  })
+);
 
 //** Routes setup */
 app.get("/", (req, res) => {
-  return res.status(200).json({ message: "welcome to extalent api services. please use /api/v1/auth routes for authentication" });
-})
-// auth routes 
+  return res.status(200).json({
+    message:
+      "welcome to extalent api services. please use /api/v1/auth routes for authentication",
+  });
+});
+// auth routes
 import authRoutes from "./routers/auth.routes.js";
 app.use("/api/v1/auth", authRoutes);
 // user routes
