9810e3c65a410536c2877bf3011e8e0fab84ffce
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-11 00:18:36 +0000

    fix(server-queue): update parameter name in genAIParser function

--- apps/server/queue/producer/aiParser.js
+++ apps/server/queue/producer/aiParser.js
@@ -1,8 +1,8 @@
 import { userInfoExtractor } from "../queueJob.js";
 
-async function genAIParser(prompt) {
-  if (!prompt) return;
-  const job = await userInfoExtractor.add("genAIParser", { prompt });
+async function genAIParser(rowText) {
+  if (!rowText) return;
+  const job = await userInfoExtractor.add("genAIParser", { rowText });
   console.log(`job added with id: ${job.id}`);
   return { jobId: job.id, message: "gen ai parse job added" };
 }
