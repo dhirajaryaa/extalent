688b25c7d30c84512680d533fb775f1cf11bcb63
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-11 00:19:37 +0000

    fix(server): file deleted after text extraction

--- apps/server/services/resume.service.js
+++ apps/server/services/resume.service.js
@@ -5,7 +5,8 @@ const parseResume = async (fileLocalPath) => {
   if (!fileLocalPath) return;
   const bufferPdf = await fs.readFile(fileLocalPath);
   const data = await pdf(bufferPdf);
-  
+  // remove after completed parser 
+  await fs.unlink(fileLocalPath);
   return data.text;
 };
 export default parseResume;
