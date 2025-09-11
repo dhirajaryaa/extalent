cdb8717033a461b76dec5e27f1bc239ff79f6e2e
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-11 22:48:14 +0000

    refactor(server-queue): remove unused AI parser and GitHub stats fetching files

--- apps/server/queue/queueJob.js
+++ apps/server/queue/queueJob.js
@@ -1,25 +1,19 @@
 import { Queue, QueueEvents} from "bullmq";
 import redisClient from "../config/redis.js";
-import genAIParser from "./producer/aiParser.js";
 
 const userInfoExtractor = new Queue("user-info-extractor", {
   connection: redisClient,
 });
 const userInfoExtractorEvent = new QueueEvents("user-info-extractor");
 
-// setup listener for queue events
-userInfoExtractorEvent.on("completed",async ({jobId,returnvalue}) => {
-const  jobName =  await userInfoExtractor.getJob(jobId);
-if(jobName.name === 'pdfParser'){
-  await genAIParser(returnvalue)
-};
-if(jobName.name === 'genAIParser'){
 
-  console.log("ai data returned");
-  
-  // console.log(returnvalue);
-};
 
+userInfoExtractorEvent.on("failed", ({ jobId, failedReason }) => {
+  console.log(`Job ${jobId} failed: ${failedReason}`);
 });
+userInfoExtractorEvent.on("completed", ({ jobId, returnvalue }) => {
+  console.log(`Job ${jobId} completed`);
+});
+
 
 export { userInfoExtractor, userInfoExtractorEvent };
