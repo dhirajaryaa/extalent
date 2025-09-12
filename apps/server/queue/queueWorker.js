30d4b4b3637ae5721703f85b5a03771aef368c73
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-12 01:20:18 +0000

    feat(server-queue0: worker code extent for handle save db job

--- apps/server/queue/queueWorker.js
+++ apps/server/queue/queueWorker.js
@@ -3,7 +3,12 @@ import redisClient from "../config/redis.js";
 import parseResume from "../services/resume.service.js";
 import { extractUserInfo } from "../prompt/user.prompt.js";
 import genAI from "../services/genai.service.js";
-import { githubUserReadme } from "../services/github.service.js";
+import {
+  githubRepoInfo,
+  githubUserInfo,
+  githubUserReadme,
+} from "../services/github.service.js";
+import { saveGithubDataInDB } from "../services/savedb.service.js";
 
 const userInfoExtractorWorker = new Worker(
   "userInfoPipeline",
@@ -11,7 +16,7 @@ const userInfoExtractorWorker = new Worker(
     switch (job.name) {
       case "User_pdf_Reader": {
         //! read resume to extract text
-        const pdfPath = job.data.pdf;
+        const pdfPath = job.data.pdfLocalFilePath;
         const rowText = await parseResume(pdfPath);
         return rowText;
       }
@@ -31,15 +36,25 @@ const userInfoExtractorWorker = new Worker(
         //! get github stats
         const data = await job.getChildrenValues();
         const username = Object.values(data)[0];
+        const repos = await githubRepoInfo(username);
         const readme = await githubUserReadme(username);
-        console.log(readme);
-        return readme;
+        const userInfo = await githubUserInfo(username);
+        return { readme, userInfo, repos };
+      }
+
+      case "Save_On_DB": {
+        const data = await job.getChildrenValues();
+        const retuned = Object.values(data)[0];
+console.log(retuned);
+
+        await saveGithubDataInDB({ readme, userInfo, repos });
+
+        return "done";
       }
     }
   },
   {
     connection: redisClient,
-    // lockDuration: 60000, // wait for 1 min before next job
   }
 );
 
