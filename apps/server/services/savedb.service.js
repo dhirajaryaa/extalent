fa90d1764fd4db7888b081a6c8c0172b5b4c950f
Dhiraj Arya <dhirajarya.ptn@gmail.com>
2025-09-12 01:20:43 +0000

    feat(server): save on db services created

---
+++ apps/server/services/savedb.service.js
@@ -0,0 +1,68 @@
+import githubModal from "../model/github.model.js";
+import userModel from "../model/user.model.js";
+
+async function saveGithubDataInDB({
+  readme: userReadme,
+  userInfo: githubUser,
+  repos: githubRepos,
+}) {
+  // store working topics
+  const topics = new Set([]);
+  githubRepos.forEach((repo) => {
+    repo.topics.forEach((topic) => {
+      topics.add(topic);
+    });
+  });
+  // top repos
+  const topRepos = githubRepos
+    ?.map((repo) => ({
+      name: repo.name,
+      description: repo.description,
+      html_url: repo.html_url,
+      stars: repo.stargazers_count,
+      size: repo.size,
+      topics: repo.topics,
+      languages: repo.languages,
+      defaultBranch: repo.default_branch,
+      recentActivity: repo.pushed_at,
+      score: (
+        repo.stargazers_count * 2 +
+        repo.forks_count * 1.5 +
+        repo.size * 0.01
+      ).toFixed(2),
+    }))
+    .sort((a, b) => b.score - a.score)
+    .slice(0, 5);
+  // languages co
+  const languageCount = {};
+  githubRepos.forEach((repo) => {
+    if (repo.language) {
+      languageCount[repo.language] = (languageCount[repo.language] || 0) + 1;
+    }
+  });
+  const topLanguages = Object.entries(languageCount).map(([lang, count]) => ({
+    name: lang,
+    repos: count,
+  }));
+
+  // save in db
+  const updateGithub = await githubModal.findByIdAndUpdate(
+    github._id,
+    {
+      name: githubUser.name,
+      username: githubUser.username,
+      bio: githubUser.bio,
+      location: githubUser.location,
+      blog: githubUser.blog,
+      publicRepoCount: githubUser.public_repos,
+      readme: userReadme,
+      languages: topLanguages,
+      userWorkTopics: Array.from(topics),
+      topRepos,
+    },
+    { new: true }
+  );
+}
+
+
+export {saveGithubDataInDB};
